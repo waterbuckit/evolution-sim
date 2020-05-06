c4c7ae59fde30380e30f16574192b4e82a5bab7f
waterbucket <adamroyal232@yahoo.com>
2020-05-07 00:48:52 +0100

    Created static method for procreation and added new necessary methods

--- src/sim/components/Creature.java
+++ src/sim/components/Creature.java
@@ -2,11 +2,23 @@ package sim.components;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import sim.components.creatures.StandardCreature;
+import sim.utils.QuadTree;
+
+import java.util.ArrayList;
 
 public interface Creature {
+    static Creature procreate(StandardCreature standardCreature, Creature creature) {
+        return null;
+    }
+
     public void render(GraphicsContext gc);
     public Creature update(Canvas canvas);
     public double getHealth();
     public double getX();
     public double getY();
+
+    ArrayList<Food> intersectFood(QuadTree quadTree);
+
+    ArrayList<Creature> intersectCreature(QuadTree quadTree);
 }
